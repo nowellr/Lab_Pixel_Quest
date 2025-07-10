250f3806ea8cb547bb969cecb5be8ec304fcfcef
nowellr <nowellr@nycstudents.net>
2025-07-10 12:15:57 -0400

    more programming
    velocity, trigger, collisions, etc

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -1,20 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     int varOne = 3;
+    public int speed = 3;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         string varTwo = "World";
         Debug.Log(varOne + varTwo);
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(varOne++);
+        float xInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
+    }
+        /*
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 1, 0);
+        }
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -1, 0);
+        }
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }*/
+
+        private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Hit");
     }
 }
+
